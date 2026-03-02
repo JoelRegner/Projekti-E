cd6a7c2b2095cb04bab288892ee7fe189b909f80
tejinder21 <tejindekumar@myy.haaga-helia.fi>
2026-03-02 13:20:53 +0200

    ..

--- projekti/src/main/java/ohjelmistoprojekti1/projekti/repository/EventRepository.java
+++ projekti/src/main/java/ohjelmistoprojekti1/projekti/repository/EventRepository.java
@@ -1,8 +1,15 @@
 package ohjelmistoprojekti1.projekti.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import ohjelmistoprojekti1.projekti.domain.Event;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
+
+    List<Event> findByNameContainingIgnoreCase(String name);
+
+    List<Event> findByCityContainingIgnoreCase(String city);
+    
 }
 
