04c8895210a1e9eceaec62a0ff986bbf14a3e889
tejinder21 <tejindekumar@myy.haaga-helia.fi>
2026-03-24 14:00:06 +0200

    Autentikointi

---
+++ projekti/src/main/java/ohjelmistoprojekti1/projekti/config/SecurityConfig.java
@@ -0,0 +1,57 @@
+package ohjelmistoprojekti1.projekti.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable()) // helpottaa Postman-testausta
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/**").authenticated()
+                .anyRequest().permitAll()
+            )
+            .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager users(PasswordEncoder passwordEncoder) {
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder.encode("admin123"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails seller = User.builder()
+                .username("seller")
+                .password(passwordEncoder.encode("seller123"))
+                .roles("LIPUNMYYJA")
+                .build();
+
+        UserDetails checker = User.builder()
+                .username("checker")
+                .password(passwordEncoder.encode("checker123"))
+                .roles("OVITARKASTAJA")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, seller, checker);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
